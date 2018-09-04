a491c01a15acf466f858fea6865788e90f4b7647
Prince <ekemini.ekanim@yahoo.com>
2018-09-05 00:32:12 +0100

    Souncloud section fixed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var backdrop = document.querySelector('.backdrop');
 var toggle = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
+var heading = document.querySelector('h4');
 
 
 
@@ -14,3 +15,21 @@ backdrop.addEventListener('click',function(){
     mobileNav.classList.remove('open');
     backdrop.classList.remove('open');
 });
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  function changeColor(){
+      colorInput = getRandomColor();
+      heading.style.color = colorInput;
+  }
+
+  setInterval("changeColor()",500);
+  
+  
